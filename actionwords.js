f90257122ce14b7eb579e9fc233d7b46b9035b41
shilpi85 <shilpi.rauniyar@gmail.com>
2016-11-22 15:31:15 -0500

    second commit

---
+++ actionwords.js
@@ -0,0 +1,25 @@
+
+var chai = require('chai');
+var chaiAsPromised = require('chai-as-promised');
+chai.use(chaiAsPromised);
+var expect = require('chai').expect;
+
+var Actionwords = {
+  iOpenP1: function (p1) {
+    browser.get(p1);
+  },
+  iWantToDoP1P2P3: function (p1, p2, p3) {
+    element(by.model('first')).sendKeys(p1);
+    element(by.model('operator')).sendKeys(p2);
+    element(by.model('second')).sendKeys(p3);
+    element(by.id('gobutton')).click();
+  },
+  resultOfOperationShouldEqualP1: function (p1) {
+    browser.sleep(5000);
+    element(by.binding('result.value')).getText().then(function(value){
+      console.log('Value of Result is: ' + value);
+      expect(value).to.equal(p1);
+    });
+  }
+};
+module.exports = Actionwords;
---
+++ project_test.js
@@ -0,0 +1,25 @@
+var Actionwords = require('./Actionwords');
+
+describe('firstProtractorTest', function () {
+  beforeEach(function () {
+    this.actionwords = Object.create(Actionwords);
+  });
+
+  it('Subtraction', function () {
+    // Given I open "https://juliemr.github.io/protractor-demo/"
+    this.actionwords.iOpenP1("https://juliemr.github.io/protractor-demo/");
+    // When I want to do "5" "-" "2"
+    this.actionwords.iWantToDoP1P2P3("5", "-", "2");
+    // Then result of operation should equal "3"
+    this.actionwords.resultOfOperationShouldEqualP1("3");
+  });
+
+  it('newAdditionTest', function () {
+    // Given I open "https://juliemr.github.io/protractor-demo/"
+    this.actionwords.iOpenP1("https://juliemr.github.io/protractor-demo/");
+    // When I want to do "1" "+" "4"
+    this.actionwords.iWantToDoP1P2P3("1", "+", "4");
+    // Then result of operation should equal "5"
+    this.actionwords.resultOfOperationShouldEqualP1("5");
+  });
+});
