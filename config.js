1547cc3a2b852a91e3c41e7b51bdfc21130266ed
SHAMIM AHMED <shamim.ahmed@nih.gov>
2016-11-22 21:55:05 -0500

    updated config document

--- config.js
+++ config.js
@@ -2,6 +2,13 @@
 
 exports.config = {
 
+    capabilities: {
+        'browserName' : 'firefox',
+        shardTestFiles: true,
+        maxInstances: 1
+
+    },
+
     seleniumAddress: 'http://localhost:4444/wd/hub',
 
     specs: ['project_test.js']
